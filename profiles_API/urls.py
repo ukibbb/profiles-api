4f6f282238ed25c2a65e9636d801c74b77cdcdef
ukibbb <lucas.bulinski@gmail.com>
2019-12-31 19:55:41 +0100

    Testing ViewSet in Django rest framework

--- profiles_API/urls.py
+++ profiles_API/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_API import views
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
